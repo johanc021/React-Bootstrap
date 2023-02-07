dac4c97b52518c525106170db3f4fadd5f89911b
johan021 <facjohan@hotmail.com>
2023-02-07 13:50:54 -0500

    Repo Inicial

---
+++ src/Components/Navegacion/Navegacion/Navegacion.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+
+const Navegacion = () => {
+	return (
+		<Navbar bg="primary" variant="dark" expand="lg" style={{ maxWidth: "1600px", margin: "0 auto" }}>
+			<Container>
+				<Navbar.Brand href="#home">Mi sitio</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="mr-auto">
+						<Nav.Link href="#home">Inicio</Nav.Link>
+						<Nav.Link href="#link">Acerca de</Nav.Link>
+						<NavDropdown title="Servicios" id="basic-nav-dropdown">
+							<NavDropdown.Item href="#action/3.1">Servicio 1</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.2">Servicio 2</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.3">Servicio 3</NavDropdown.Item>
+							<NavDropdown.Divider />
+							<NavDropdown.Item href="#action/3.4">Separador</NavDropdown.Item>
+						</NavDropdown>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+)};
+
+export default Navegacion;
---
+++ src/Destructuring/spreadDestructuring.js
@@ -0,0 +1,21 @@
+//Destructuring de objetos
+const animal = {
+    patas: 4,
+    maulla: true,
+    visionNocturna:true
+}
+
+console.log(animal.patas);
+
+
+//acceder a la propiedad a traves de una variable
+
+const { maulla } = animal
+
+console.log(maulla)
+
+
+//Destructuring de arrays
+
+const frutas = ['pera', 'fresa', 'banana', 'frutilla', 'anon']
+
